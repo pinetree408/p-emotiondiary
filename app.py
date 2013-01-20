8f7de55ac0c245a2cbba762e8d1a01a3e8c5e4b3
Shaun Park <shaun.park@kaist.ac.kr>
2013-01-21 05:06:49 +0900

    temp

--- app.py
+++ app.py
@@ -190,7 +190,7 @@ def test():
         #         score.append(int(scoreItem))
         # userCache[sessionID]['scores'] = Test('CESD', 10, 20130118220015)
         # flash("You're score is " +str(score)+ " points.",'system')
-        return render_template('feedback.html', user=userCache[sessionID])
+        return render_template('feedback.html', testScore=Scores)
 
 @app.route('/userSession/')
 def userSession():
