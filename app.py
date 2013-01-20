54b61a04bb93cf03f7838718dc114b1c167c8810
Shaun Park <shaun.park@kaist.ac.kr>
2013-01-21 03:55:09 +0900

    temp

--- app.py
+++ app.py
@@ -172,12 +172,12 @@ def test():
     #     #Store test scores at TEST NAME (which is returned)
     #     #Load an outgoing URL
 
-        # score = []
-        # for i in range(len(questions)):
-        #     scoreItem = eval("request.form.get('var" + str(i) + "')")
-        #     if scoreItem:
-        #         score.append(int(scoreItem))
-        # userCache[sessionID]['scores']['CESD1'] = Test('CESD', int(sum(score)), time.time())
+        score = []
+        for i in range(len(questions)):
+            scoreItem = eval("request.form.get('var" + str(i) + "')")
+            if scoreItem:
+                score.append(int(scoreItem))
+        userCache[sessionID]['scores']['CESD1'] = Test('CESD', int(sum(score)), time.time())
         # flash("You're score is " +str(score)+ " points.",'system')
         return render_template('feedback.html', user=userCache[sessionID])
 
