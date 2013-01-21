302c01b0fae251af659f27ef2929540b4aeba0b9
Shaun Park <shaun.park@kaist.ac.kr>
2013-01-21 16:54:36 +0900

    temp

--- app.py
+++ app.py
@@ -97,7 +97,8 @@ def login():
 def database(): #A function to render raw data - can be improved later
     # return pprint.pformat(Tips) #For rendering Tips
     # return pprint.pformat(User.query.all()) #For rendering User DB
-    return pprint.pformat(userCache.sessionID) #For rendering userCache
+    # return pprint.pformat(userCache) #For rendering userCache
+    return pprint.pformat(userCache) #For rendering userCache
 
 @app.route('/about')
 def about():
