10e922569f181a4a4c3ea2dbc0a3ca0f46d82520
Shaun Park <shaun.park@kaist.ac.kr>
2013-01-25 06:44:44 +0900

    temp

--- app.py
+++ app.py
@@ -186,7 +186,7 @@ def test():
         #     scoreItem = eval("request.form.get('var" + str(i) + "')")
         #     if scoreItem:
         #         score.append(int(scoreItem))
-        # userCache[sessionID].testscores = 10
+        userCache[sessionID][testscores] = 10
         # flash("You're score is " +str(score)+ " points.",'system')
         return render_template('feedback.html', user=userCache[sessionID])
 
