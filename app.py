a58b78fd12005f76c3197d78f3d1bef5e8cacd76
Shaun Park <shaun.park@kaist.ac.kr>
2013-01-29 08:57:54 +0900

    'temp'

--- app.py
+++ app.py
@@ -171,7 +171,7 @@ def userInfo():
     #     answer = None
     #     return render_template('tips.html', user=userCache[sessionID], tip=tip)
 
-@app.route('/tips', methods=['GET', 'POST'])
+@app.route('/tips')
 def tips():
     sessionID = get_facebook_oauth_token()
     return render_template('game.html', user=userCache[sessionID])
