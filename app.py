fa40eed3551912b8f010dfe8e93f033031eea1a0
Shaun Park <shaun.park@kaist.ac.kr>
2013-01-28 18:22:00 +0900

    temp

--- app.py
+++ app.py
@@ -240,7 +240,7 @@ def userSession():
         # crawlData = [timelineFeed.data, me.data['relationship_status'], groups.data, interest.data, likes.data, location.data, notes.data, message.data, friendRequest.data, events.data]
         
         # newUser = User(sessionID[0], me.data['id'], me.data['name'], me.data['locale'], len(friends.data['data']), 'control', 1, {}, {}, crawlData)
-        # newUser = User(sessionID[0], me.data['id'], me.data['name'], me.data['locale'], len(friends.data['data']))
+        newUser = O.User(sessionID[0], me.data['id'], me.data['name'], me.data['locale'], len(friends.data['data']))
         # try:
         #     db.session.add(newUser)
         #     db.session.commit()
