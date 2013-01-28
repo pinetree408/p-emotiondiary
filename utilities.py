cd6df2cd8579703d743b45ed7049b826fc6e0d9d
Shaun Park <shaun.park@kaist.ac.kr>
2013-01-28 21:33:01 +0900

    'temp'

--- utilities.py
+++ utilities.py
@@ -14,8 +14,10 @@ TrapErrors = False #Toggles some error handling tools
 #Setting up the appropriate Facebook session
 if DEBUG == True:
 	#Debug app codes
-	FACEBOOK_APP_ID = '292670767512606'
-	FACEBOOK_APP_SECRET = 'c8bf8a30da9fcb60b188cd196850ea47'
+	# FACEBOOK_APP_ID = '292670767512606'
+	# FACEBOOK_APP_SECRET = 'c8bf8a30da9fcb60b188cd196850ea47'
+	FACEBOOK_APP_ID = '395527847191253'
+	FACEBOOK_APP_SECRET = 'a22ce24a9cfe6f266364bfa2942e7f6b'
 	OFFLINE = True
 else: 
 	#Live app codes
