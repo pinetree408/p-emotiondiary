bf54da2467e3cca68f03af6377ebbc880b54fcdf
Shaun Park <shaun.park@kaist.ac.kr>
2013-01-28 17:36:30 +0900

    temp

--- app.py
+++ app.py
@@ -37,11 +37,12 @@ class User(db.Model):
     name = db.Column(db.String(80))
     locale = db.Column(db.String(80))
     friendNum = db.Column(db.Integer)
-    target = db.Column(db.String(40))
-    points = db.Column(db.Integer)
-    testscore = db.Column(db.String(100))
-    tip = db.Column(db.String(100))
-    crawlData = db.Column(db.String(65535))
+    # target = db.Column(db.String(40))
+    # points = db.Column(db.Integer)
+    # testscore = db.Column(db.String(100))
+    # tip = db.Column(db.String(100))
+    # crawlData = db.Column(db.String(65535))
+
     # dateAdded: time.time(),
     # friends: len(friends.data['data']),
     # points: 1,
@@ -50,17 +51,18 @@ class User(db.Model):
     # scores:{},
     # tips:{} #tip ID keys with answers as values
 
-    def __init__(self, authID, facebookID, name, locale, friendNum, target, points, testscore, tip, crawlData):
+    # def __init__(self, authID, facebookID, name, locale, friendNum, target, points, testscore, tip, crawlData):
+    def __init__(self, authID, facebookID, name, locale, friendNum):
         self.authID = authID
         self.facebookID = facebookID
         self.name = name
         self.locale = locale
         self.friendNum = friendNum
-        self.target = target
-        self.points = points
-        self.testscore = testscore
-        self.tip = tip
-        self.crawlData = crawlData
+        # self.target = target
+        # self.points = points
+        # self.testscore = testscore
+        # self.tip = tip
+        # self.crawlData = crawlData
 
     def __repr__(self):
         return str(self.name) + ' ' + str(self.authID)
@@ -222,21 +224,23 @@ def userSession():
         #The user does not exist. Lets create them
         me = facebook.get('/me')
         friends = facebook.get('/me/friends')
-        timelineFeed = facebook.get('/me/feed')
-        groups = facebook.get('/me/groups?fields=name')
-        interest = facebook.get('/me/interests')
-        likes = facebook.get('/me/likes?fields=name')
-        location = facebook.get('/me/locations?fields=place')
-        notes = facebook.get('me/notes')
-        messages = facebook.get('me/inbox?fields=comments')
-        friendRequest = facebook.get('me/friendrequests?fields=from')
-        events = facebook.get('me/events')
+
+        # timelineFeed = facebook.get('/me/feed')
+        # groups = facebook.get('/me/groups?fields=name')
+        # interest = facebook.get('/me/interests')
+        # likes = facebook.get('/me/likes?fields=name')
+        # location = facebook.get('/me/locations?fields=place')
+        # notes = facebook.get('me/notes')
+        # messages = facebook.get('me/inbox?fields=comments')
+        # friendRequest = facebook.get('me/friendrequests?fields=from')
+        # events = facebook.get('me/events')
 
         #Instantiate user in database
         
-        crawlData = [timelineFeed.data, me.data['relationship_status'], groups.data, interest.data, likes.data, location.data, notes.data, message.data, friendRequest.data, events.data]
+        # crawlData = [timelineFeed.data, me.data['relationship_status'], groups.data, interest.data, likes.data, location.data, notes.data, message.data, friendRequest.data, events.data]
         
-        newUser = User(sessionID[0], me.data['id'], me.data['name'], me.data['locale'], len(friends.data['data']), 'control', 1, {}, {}, crawlData)
+        # newUser = User(sessionID[0], me.data['id'], me.data['name'], me.data['locale'], len(friends.data['data']), 'control', 1, {}, {}, crawlData)
+        newUser = User(sessionID[0], me.data['id'], me.data['name'], me.data['locale'], len(friends.data['data']))
         try:
             db.session.add(newUser)
             db.session.commit()
@@ -266,9 +270,10 @@ def get_facebook_oauth_token():
     if OFFLINE:
         return 'Debug Mode'
     try: 
-        short_token = session.get('oauth_token')
-        extended_token = get_extended_access_token(short_token, FACEBOOK_APP_ID, FACEBOOK_APP_SECRET)
-        return extended_token[0]
+        return session.get('oauth_token')
+        # short_token = session.get('oauth_token')
+        # extended_token = get_extended_access_token(short_token, FACEBOOK_APP_ID, FACEBOOK_APP_SECRET)
+        # return extended_token[0]
     except ValueError:
         pass
     return None
