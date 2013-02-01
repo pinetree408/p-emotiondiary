fe2638d33dd55ee33be6d1c13bf53ebf6b411a8f
Shaun Park <shaun.park@kaist.ac.kr>
2013-02-01 23:19:39 +0900

    'temp'

--- app.py
+++ app.py
@@ -230,7 +230,7 @@ def game():
     sessionID = get_facebook_oauth_token()
 
     tempUser = O.User(userCache[sessionID].name, userCache[sessionID].id, sessionID, userCache[sessionID].dateAdded, userCache[sessionID].friends,
-                               userCache[sessionID].points + 2, userCache[sessionID].locale, userCache[sessionID].target, userCache[sessionID].testscores,
+                               userCache[sessionID].points + 0, userCache[sessionID].locale, userCache[sessionID].target, userCache[sessionID].testscores,
                                userCache[sessionID].tips, userCache[sessionID].data)
                 # We can't change the value of userCache[sessionID] because it's namedtuple, the immutable object. to adjust the value, we should change the whole object.
     userCache[sessionID] = tempUser
@@ -308,7 +308,7 @@ def userSession():
     elif sessionUser != None:
         #Returning user :: The user exists in DB. apply user to cache and show them a game
         userCache[sessionID] = O.User(sessionUser.name, sessionUser.facebookID, sessionID, time.time(), sessionUser.friendNum,
-                                        sessionUser.points, sessionUser.locale, sessionUser.target, sessionUser.testscore, sessionUser.tip, sessionUser.crawldata)
+                                        sessionUser.points + 0, sessionUser.locale, sessionUser.target, sessionUser.testscore, sessionUser.tip, sessionUser.crawldata)
 
         me = facebook.get('me')
         timelineFeed = facebook.get('me/feed')
