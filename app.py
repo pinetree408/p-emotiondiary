8f9e3ec9bbd817268237c559c78f69745cc2e567
Shaun Park <shaun.park@kaist.ac.kr>
2013-02-01 19:15:45 +0900

    'temp'

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+# -*- coding:utf-8 -*-
+
 #Packages to include
 from flask import Flask, request, session, url_for, redirect, render_template, abort, g, flash
 from flask.ext.sqlalchemy import SQLAlchemy
